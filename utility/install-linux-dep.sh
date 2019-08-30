5a204ff1f4457869fb79fcc5eb010d99f82d9152
Etuka Onono <eonono@persistentnotions.co.uk>
2019-08-30 17:18:01 +0100

    refactor to installation script.

--- utility/install-linux-dep.sh
+++ utility/install-linux-dep.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+apt-get update 
+apt-get install -y git
+apt-get install -y gcc g++
+apt-get install -y cmake
+rm -rf /var/lib/apt/lists
 git clone https://github.com/catchorg/Catch2.git
+
 cd Catch2
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
 sudo cmake --build build/ --target install
