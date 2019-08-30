bccbb530246ad1d25eff8cbff5cec84884dc17cd
Etuka Onono <eonono@persistentnotions.co.uk>
2019-08-30 23:20:55 +0100

    refactor, added a test generator.

--- src/main/sim/simulation.h
+++ src/main/sim/simulation.h
@@ -5,15 +5,41 @@
 namespace vol {  
   
   struct Generator {
-    // build normal generator
-    static auto normal(const double mu, const double sigma) {
+    
+    template<typename gen_type, typename dist_type>
+    static auto create(dist_type&& adapter) {
       std::random_device rd{};
-      std::mt19937_64 gen(rd());
-      auto norm = std::normal_distribution<>(mu, sigma);
-      return [norm, gen]() mutable { double result = (norm)(gen); return result;};
-    };
+      gen_type gen(rd());
+      return [adapter, gen]() mutable { return adapter(gen);};
+    }
 
+    // generators
+    static auto normal(double mu, double sigma) {
+      typedef std::normal_distribution<>  norm;
+      return create<std::mt19937_64, norm>(norm(mu, sigma));
+    }
+    
+    static auto lognormal(double nu, double sigma) {
+      typedef std::lognormal_distribution<> lognorm;
+      return create<std::mt19937_64, lognorm>(lognorm(nu, sigma));
+    }
+
+    //test generator
+    static auto constant(const double level) {
+      struct const_impl {
+        const double level_;
+        //hack constructors to minimise typage.
+        const_impl(std::random_device&): level_(0.0) {}
+        const_impl(const double level): level_(level) {}
+
+        double operator()(const const_impl&) {
+          return level_;
+        }
+      };
+      return create<const_impl, const_impl>(const_impl(level));
+    }
   };
+
 
 
 }
--- src/test/simulation-test.cpp
+++ src/test/simulation-test.cpp
@@ -10,13 +10,27 @@ SCENARIO ("Simulation pipelines are composed functions.", "[simulation]") {
     }
   }
 
-  WHEN("Generator::normal constructs a function.") {
+  WHEN("a generator is constructed") {
     auto fn = vol::Generator::normal(1, 1);
+    auto lfn = vol::Generator::lognormal(1,2);
 
     THEN("No two calls to the function are identical") {
-      double first = fn();
-      double second = fn();
-      REQUIRE(first != second);
+      REQUIRE(fn() != fn());
+      REQUIRE(lfn() != lfn());
     }
   }
+
+  WHEN("a constant test generator is constructed") {
+    auto const1 = vol::Generator::constant(1.0);
+    auto const2 = vol::Generator::constant(2.0);
+
+    THEN("each call to the generator is identical") {
+      REQUIRE(const1() == const1());
+      REQUIRE(const2() == const2());
+      REQUIRE(const1() == 1);
+      REQUIRE(const2() == 2);
+    }
+
+  }
+
 }
