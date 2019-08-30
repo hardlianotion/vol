9bd68ba86751e2915ac250976e2a938309962ed1
Etuka Onono <eonono@persistentnotions.co.uk>
2019-08-30 14:22:00 +0100

    Looks like c++ lambdas are actually useful?

--- src/test/simulation-test.cpp
+++ src/test/simulation-test.cpp
@@ -1,4 +1,12 @@
 #include "catch2/catch.hpp"
 #include "sim/simulation.h"
 
+SCENARIO ("lambdas can be passed to variables.", "[simulation]") {
+  WHEN("a lambda is passed to an auto var") {
+    auto fn = [](int x) {return x + 1;};
 
+    THEN("The var can be used as a function") {
+      REQUIRE(fn(1) == 2);
+    }
+  }
+}
