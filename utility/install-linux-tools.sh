7c7a524af9297b6789eeb65b8e0aaa9a6d1276f0
Etuka Onono <eonono@persistentnotions.co.uk>
2019-08-30 17:29:39 +0100

    try again,

--- utility/install-linux-tools.sh
+++ utility/install-linux-tools.sh
@@ -9,4 +9,4 @@ git clone https://github.com/catchorg/Catch2.git
 
 cd Catch2
 cmake -Bbuild -H. -DBUILD_TESTING=OFF
-sudo cmake --build build/ --target install
+cmake --build build/ --target install
