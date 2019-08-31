4abc715bc573ff10d11c16bd061ea51f634612b5
Etuka Onono <eonono@persistentnotions.co.uk>
2019-08-31 16:29:39 +0100

    try again.

--- utility/install-linux-tools.sh
+++ utility/install-linux-tools.sh
@@ -9,13 +9,13 @@ git clone --branch v2.9.2 --depth 1 https://github.com/catchorg/Catch2.git
 
 #install test framework
 cd Catch2
-cmake -Bbuild -H. -DBUILD_TESTING=OFF
+#cmake -Bbuild -H. -DBUILD_TESTING=OFF
 cmake --build build/ --target install
 
 #install ranges
 cd ..
 git clone --branch 0.9.0 --depth 1 https://github.com/ericniebler/range-v3.git
 cd range-v3
-cmake -Bbuild -H. -DBUILD_TESTING=OFF
+#cmake -Bbuild -H. -DBUILD_TESTING=OFF
 cmake --build build/ --target install
 
